0bd2d450b4c3f97f45ba27829ec820064c05f6fb
Gopalpatidar0012 <yogapuran0012@gmail.com>
2021-09-07 17:55:10 +0530

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+ const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissor = document.querySelector('#scissor')
+const board = document.querySelector("#board")
+
+
+const handler = (e) => {
+    const action = e.target.id
+
+    switch (action) {
+        case "rock":
+            board.innerHTML = "Rock crushes scissors"
+            break
+        case 'paper':
+            board.innerHTML = "Paper traps Rock"
+            break
+        case 'scissor':
+            board.innerHTML = "Scissor cuts paper"
+            break
+        default:
+            board.innerHTML = "Click something"
+    }
+
+}
+
+
+
+rock.addEventListener('click', handler)
+paper.addEventListener('click', handler)
+scissor.addEventListener('click', handler)
