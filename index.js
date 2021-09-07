1b2b72ae4d9ed198b447c9d6c809391c20691973
Gopalpatidar0012 <yogapuran0012@gmail.com>
2021-09-07 18:32:52 +0530

    first one

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
- const rock = document.querySelector('#rock')
-const paper = document.querySelector('#paper')
-const scissor = document.querySelector('#scissor')
-const board = document.querySelector("#board")
-
-
-const handler = (e) => {
-    const action = e.target.id
-
-    switch (action) {
-        case "rock":
-            board.innerHTML = "Rock crushes scissors"
-            break
-        case 'paper':
-            board.innerHTML = "Paper traps Rock"
-            break
-        case 'scissor':
-            board.innerHTML = "Scissor cuts paper"
-            break
-        default:
-            board.innerHTML = "Click something"
-    }
-
-}
+const balls=document.querySelectorAll(".ball")
+const body=document.querySelectorAll("body");
 
 
 
-rock.addEventListener('click', handler)
-paper.addEventListener('click', handler)
-scissor.addEventListener('click', handler)
+const arr = [1,2,3,4,5]
+ arr.push(5)
+console.log(arr);
+arr.shift(2)//delete first value
+console.log(arr);
+arr.unshift(5);//add o index of 5 [5,1,2,3,4,5]
+console.log(arr);
+arr.splice(1,3);//delete 1 index to 3 index value  [1,5]
+console.log(arr);
+
+// while(arr.length<15){
+
+   
+    
+// }
+// console.log(arr.indexOf('1'));
+//console.log(arr);
+// console.log(arr.indexOf('1'))
+// console.log(arr.indexOf('1'))
+// console.log(arr.indexOf('1'))
+
